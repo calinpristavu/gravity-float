efaa3286fe7d50f122d29cd22ca1060a3118c82e
Ovidiu Enache <i.ovidiuenache@yahoo.com>
2017-07-14 20:39:09 +0300

    [FLOAT-7] Added the search voucher functionality

---
+++ app/DoctrineMigrations/Version20170714172035.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170714172035 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE vouchers ADD voucher_code VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_931507487678B020 ON vouchers (voucher_code)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_931507487678B020 ON vouchers');
+        $this->addSql('ALTER TABLE vouchers DROP voucher_code');
+    }
+}
---
+++ src/AppBundle/Form/Type/SearchVoucherType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class SearchVoucherType
+ * @author: Enache Ioan Ovidiu <i.ovidiuenache@yahoo.com>
+ */
+class SearchVoucherType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('vouchercode')
+            ->add('search', SubmitType::class, array('label' => "button.search"))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => null,
+        ));
+    }
+}
