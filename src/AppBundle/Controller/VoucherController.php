70332a220cd3c9334aa726235267a7d7aa7f9efa
Ovidiu Enache <i.ovidiuenache@yahoo.com>
2017-07-16 00:36:08 +0300

    [FLOAT-10] Fixed and refactored vouhcer filtering and pagination

--- src/AppBundle/Controller/VoucherController.php
+++ src/AppBundle/Controller/VoucherController.php
@@ -6,7 +6,6 @@ use AppBundle\Form\Type\SearchVoucherType;
 use AppBundle\Form\Type\VoucherDateType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -77,113 +76,48 @@ class VoucherController extends Controller
      */
     public function vouchersAction(Request $request)
     {
+        if (!$request->get('page')) {
+            $request->request->set('page', 1);
+        }
+
         $form = $this->createForm(VoucherDateType::class);
         $form->handleRequest($request);
         if ($form->isValid()) {
-            $request->request->remove('created_at');
-            $request->request->set('created_at', $form->getData()['created_at']);
+            $request->request->set('searchField', $form->getData()['created_at']);
         }
 
-        if ($request->get('created_at') !== null) {
-            return $this->filteredVouchers($request, $form);
+        $filters = [
+            'page' => (int)$request->get('page'),
+            'items_per_page' => self::$NUMBER_OF_VOUCHERS_PER_PAGE,
+        ];
+
+        $voucherFinder = $this->get('voucher.finder');
+        if ($request->get('searchField') !== null) {
+            $filters['created_at'] = $request->get('searchField');
         }
 
-        return $this->allVouchers($request, $form);
-    }
-
-    protected function filteredVouchers(Request $request, Form $form)
-    {
-        $requestParam = $request->get('created_at');
-        $year = $requestParam === 'CURRENT_YEAR' ? date("Y") : date("Y", strtotime("-1 year"));
-        $filteredVouchersCount = $this->getDoctrine()
-            ->getRepository('AppBundle:Voucher')
-            ->countAllFiltered($year);
-
-        $page = $this->validatePageNumber($request->get('page'), $filteredVouchersCount);
-        $offset = self::$NUMBER_OF_VOUCHERS_PER_PAGE*($page - 1);
-        $currentPageVouchers = $this->getDoctrine()
-            ->getRepository('AppBundle:Voucher')
-            ->findAllFilteredFromPage($offset, self::$NUMBER_OF_VOUCHERS_PER_PAGE, $year)
-        ;
-        $shops = $this->getDoctrine()->getRepository('AppBundle:Shop')->findAll();
+        $vouchers = $voucherFinder->setFilters($filters)->getVouchers();
+        $allVouchersCount = $voucher = $this->getDoctrine()->getRepository('AppBundle:Voucher')->countAll();
+        $nrOfPages = (int)($allVouchersCount / self::$NUMBER_OF_VOUCHERS_PER_PAGE) + 1;
+        if ($allVouchersCount % self::$NUMBER_OF_VOUCHERS_PER_PAGE == 0) {
+            $nrOfPages = $allVouchersCount / self::$NUMBER_OF_VOUCHERS_PER_PAGE;
+        }
 
         return $this->render('floathamburg/vouchers.html.twig',[
-            'vouchers' => $currentPageVouchers,
-            'shops' => $shops,
-            'hasNextPage' => $this->validatePageNumber($page + 1, $filteredVouchersCount) == 1 ? false : true,
-            'hasPreviousPage' => ($this->validatePageNumber($page - 1, $filteredVouchersCount) == 1 && $page != 2) ? false : true,
-            'currentPage' => $page,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    protected function allVouchers(Request $request, Form $form)
-    {
-        $allVouchersCount = $this->getDoctrine()->getRepository('AppBundle:Voucher')->countAll();
-
-        $page = $this->validatePageNumber($request->get('page'), $allVouchersCount);
-        $offset = self::$NUMBER_OF_VOUCHERS_PER_PAGE*($page - 1);
-        $currentPageVouchers = $this->getDoctrine()
-            ->getRepository('AppBundle:Voucher')
-            ->findAllFromPage($offset, self::$NUMBER_OF_VOUCHERS_PER_PAGE);
-        $shops = $this->getDoctrine()->getRepository('AppBundle:Shop')->findAll();
-        return $this->render('floathamburg/vouchers.html.twig',[
-            'vouchers' => $currentPageVouchers,
-            'shops' => $shops,
-            'hasNextPage' => $this->validatePageNumber($page + 1, $allVouchersCount) == 1 ? false : true,
-            'hasPreviousPage' => ($this->validatePageNumber($page - 1, $allVouchersCount) == 1 && $page != 2) ? false : true,
-            'currentPage' => $page,
+            'vouchers' => $vouchers,
+            'numberOfPages' => $nrOfPages,
+            'currentPage' => $request->get('page'),
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * Validates the page number.
+     * @Route("/voucher/all/reset-filters", name="voucher_all_reset_filters")
      *
-     * @param int $page
-     * @param int $items
-     *
-     * @return int the page number if valid
-     *              1 if it is not valid (the first page)
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    protected function validatePageNumber(int $page = null, int $items) : int
+    public function resetFiltersAction()
     {
-        if ($page === null || $page < 1) {
-            return 1;
-        }
-
-        //If the page number is to big compared to voucher database size
-        //First page doesn't count
-        if ($page > 1) {
-            if (($page - 1) * self::$NUMBER_OF_VOUCHERS_PER_PAGE  > $items ) {
-                return 1;
-            }
-        }
-
-        return $page;
-    }
-
-    /**
-     * @param array $formData
-     *
-     * @return array
-     */
-    protected function prepareFoundVouchers(array $formData) : array
-    {
-        $year = null;
-        switch ($formData['created_at']) {
-            case 'CURRENT_YEAR':
-                $year = date("Y");
-                break;
-            case 'LAST_YEAR':
-                $year = date("Y", strtotime("-1 year"));
-                break;
-            default:
-                break;
-        }
-
-         return $this->getDoctrine()
-            ->getRepository('AppBundle:Voucher')
-            ->findFromYear($year);
+        return $this->redirectToRoute('voucher_all', ['page' => 1]);
     }
 }
--- src/AppBundle/Repository/VoucherRepository.php
+++ src/AppBundle/Repository/VoucherRepository.php
@@ -11,21 +11,6 @@ use Doctrine\ORM\EntityRepository;
 class VoucherRepository extends EntityRepository
 {
     /**
-     * @param int $offset
-     * @param int $numberOfResults
-     *
-     * @return array
-     */
-    public function findAllFromPage(int $offset, int $numberOfResults)
-    {
-        return $this->createQueryBuilder('v')
-            ->setFirstResult($offset)
-            ->setMaxResults($numberOfResults)
-            ->getQuery()
-            ->getResult();
-    }
-
-    /**
      * @return int
      */
     public function countAll()
@@ -34,38 +19,5 @@ class VoucherRepository extends EntityRepository
             ->select('count(v.id)')
             ->getQuery()
             ->getSingleScalarResult();
-    }
-
-    /**
-     * @param string $year
-     *
-     * @return int
-     */
-    public function countAllFiltered(string $year)
-    {
-        return (int)$this->createQueryBuilder('v')
-            ->select('count(v.id)')
-            ->where('YEAR(v.creationDate) = :year')
-            ->setParameter('year', $year)
-            ->getQuery()
-            ->getSingleScalarResult();
-    }
-
-    /**
-     * @param int $offset
-     * @param int $numberOfResults
-     * @param string $year
-     *
-     * @return array
-     */
-    public function findAllFilteredFromPage(int $offset, int $numberOfResults, string $year)
-    {
-        return $this->createQueryBuilder('v')
-            ->where('YEAR(v.creationDate) = :year')
-            ->setParameter('year', $year)
-            ->setFirstResult($offset)
-            ->setMaxResults($numberOfResults)
-            ->getQuery()
-            ->getResult();
     }
 }
