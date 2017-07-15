437bec49ba3ce1b2f8dba8bcd34c0d442ae477d9
Ovidiu Enache <i.ovidiuenache@yahoo.com>
2017-07-15 12:45:11 +0300

    [FLOAT-8] Added my account feature

---
+++ src/AppBundle/Form/Type/UserType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use AppBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class UserType
+ *
+ * @author: Enache Ioan Ovidiu <i.ovidiuenache@yahoo.com>
+ */
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('phone', NumberType::class)
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'invalid_message' => 'passwords.must.match',
+                'required' => true,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('save', SubmitType::class, array('label' => "button.save"));
+        ;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
+}
