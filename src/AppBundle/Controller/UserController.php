9e8dbc17bd6ec603c776854d4e070ff60e73ac16
Ovidiu Enache <i.ovidiuenache@yahoo.com>
2017-07-15 12:45:11 +0300

    [FLOAT-8] Added my account feature

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class VoucherController
@@ -13,9 +15,30 @@ class UserController extends Controller
 {
     /**
      * @Route("/user/profile", name="user_profile")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function profileAction()
+    public function profileAction(Request $request)
     {
-        return $this->render('floathamburg/userprofile.html.twig');
+        $form = $this->createForm(UserType::class, $this->getUser());
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($this->getUser());
+            $em->flush();
+
+            return $this->render('floathamburg/userprofile.html.twig', [
+                'form' => null,
+                'submitted' => true,
+            ]);
+        }
+
+        return $this->render('floathamburg/userprofile.html.twig', [
+            'form' => $form->createView(),
+            'submitted' => false,
+        ]);
     }
 }
