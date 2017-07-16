e6408599e4191df4da49b7b42488084129cb491a
Ovidiu Enache <i.ovidiuenache@yahoo.com>
2017-07-16 22:49:25 +0300

    Removed var_dump from controller

--- src/AppBundle/Controller/VoucherController.php
+++ src/AppBundle/Controller/VoucherController.php
@@ -113,6 +113,13 @@ class VoucherController extends Controller
         $voucher->setVoucherCode('111AAA');
         $voucher->setCreationDate(new DateTime());
 
+<<<<<<< Updated upstream
+=======
+        if ($voucher->isIncludedPostalCharges()) {
+            $voucher->setOriginalValue($voucher->getOriginalValue() - 1.5);
+        }
+
+>>>>>>> Stashed changes
         $em = $this->getDoctrine()->getManager();
         $em->persist($voucher);
         $em->flush();
