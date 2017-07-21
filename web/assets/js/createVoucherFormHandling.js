60c6f2cbf9e2b5149515c3d3a4cd960a79ceca7e
Ionut Enache <ionut.enache@evozon.com>
2017-07-21 14:38:00 +0300

    [FLOAT-24] Added mandatory child form fields in voucher creation

--- web/assets/js/createVoucherFormHandling.js
+++ web/assets/js/createVoucherFormHandling.js
@@ -19,5 +19,23 @@ $(function () {
         })
     });
 
+    $( "#voucher_usages_0" ).click(function() {
+        if (!$('#voucher_usages_0').hasClass('hidden')) {
+            $("#voucher_massage_type").attr("required", true);
+        } else {
+            $("#voucher_massage_type").attr("required", false);
+        }
+    });
+
+    $( "#voucher_onlineVoucher" ).click(function() {
+        if (!$('#voucher_onlineVoucher').hasClass('hidden')) {
+            $("#voucher_orderNumber").attr("required", true);
+            $("#voucher_invoiceNumber").attr("required", true);
+        } else {
+            $("#voucher_orderNumber").attr("required", false);
+            $("#voucher_invoiceNumber").attr("required", false);
+        }
+    });
+
     JsBarcode("#barcode", $('#barcode').attr('data-code'), {});
 });
