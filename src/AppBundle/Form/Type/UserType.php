4b2116b8bc3278f1020151b09c3f40f8fe1a3327
Ionut Enache <ionut.enache@evozon.com>
2017-08-19 14:41:14 +0300

    [FLOAT-29] Updated create user form

--- src/AppBundle/Form/Type/UserType.php
+++ src/AppBundle/Form/Type/UserType.php
@@ -9,12 +9,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Class UserType
@@ -55,12 +55,18 @@ class UserType extends AbstractType
             ))
             ->add('roles', ChoiceType::class, array(
                 'choices'  => array(
-                    'regular.user' => 'ROLE_USER',
-                    'admin' => 'ROLE_ADMIN',
+                    'Regular User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
                 ),
                 'expanded' => true,
                 'label' => false,
                 'multiple' => true,
+                'constraints' => [
+                    new Assert\Count(array(
+                        'min' => 1,
+                        'minMessage' => 'Please select at least one option!',
+                    ))
+                ]
             ))
             ->add('save', SubmitType::class, array('label' => "button.save"));
         ;
